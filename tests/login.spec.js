6503670528c1d4dfd62739f35d2b6cd7b52d4136
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-02-15 21:28:13 +0530

    login page

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,6 +7,12 @@ test("Login to OrangeHRM", async({browser})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     await expect(page).toHaveTitle('OrangeHRM');
 
+    //loggin in to the web url
+
+    await page.locator("input[name='username']").fill('Admin');
+    await page.locator("input[name='password']").fill('admin123');
+    await page.locator("button[type='submit']").click();
+
     await page.pause();
 
 
