a7604a13828364e3432d9a1844e51dff2dfe9dd9
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-02-25 22:58:57 +0530

    product payment

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -10,6 +10,7 @@ test("Navigate to OrangeHRM", async({browser})=>{
     const login = page.locator('#login');
     const products = page.locator('.card-body');
     const productName = 'ZARA COAT 3'
+    const paymentForm = page.locator(".field .input.txt");
     
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     
@@ -46,15 +47,28 @@ test("Navigate to OrangeHRM", async({browser})=>{
                 console.log("Clicking the add to cart button !!");
                 await products.nth(i).locator("text= Add To Cart").click();
                 break;
-
-
-        }
+    }
     }
 
     await page.locator('button[routerlink*="cart"]').click();
     await page.locator('.cartSection').first().waitFor();
     const bool = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
     expect(bool).toBeTruthy();
+    await page.locator("button[type='button']").last().click();
+
+    //Payment page
+
+    await page.locator(".field .input.txt.text-validated").fill("1234123412341234");
+    await page.locator(".input.ddl").first().selectOption("03");
+    await page.locator(".input.ddl").last().selectOption("27");
+
+    await page.locator(".field.small .input.txt").first().pressSequentially("098");
+    await paymentForm.nth(2).fill("Test User");
+    await paymentForm.last().fill("rahulshettyacademy");
+    await page.locator(".btn.btn-primary.mt-1").click();
+    expect(page.locator(".mt-1.ng-star-inserted")).toContainText("Applied")
+
+
 
     await page.pause();
 
