26689998910df62393c3c137d56a519c56406380
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-03-02 23:29:16 +0530

    updated

--- tests/getByLocator.spec.js
+++ tests/getByLocator.spec.js
@@ -3,6 +3,9 @@ const{expect,test} = require("@playwright/test");
 test('Get By Locators',async ({page})=>{
 
     await page.goto('https://rahulshettyacademy.com/angularpractice/');
+    await page.getByLabel("Check me out if you Love IceCreams!").check();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
 
     await page.pause();
 
