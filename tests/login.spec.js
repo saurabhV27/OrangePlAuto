007b4b607e481f78b90626f362d32d95c02ff602
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-01 18:54:49 +0530

    page Object model

---
+++ tests/login.spec.js
@@ -0,0 +1,98 @@
+const{test,expect} = require("@playwright/test");
+//const { networkInterfaces } = require("node:os");
+const {loginPage}=require('../PageLocators/loginPage');
+const {dashboardPage} = require('../PageLocators/dashboardPage');
+
+
+test("Navigate to OrangeHRM", async({page})=>{
+    
+    const username = page.locator('#userEmail');
+    const password = page.locator('#userPassword');
+    const login = page.locator('#login');
+    const productName = 'ZARA COAT 3'
+    const paymentForm = page.locator(".field .input.txt");
+    const listCountry = page.locator('.ta-results.list-group.ng-star-inserted span');
+    const nameCountry = " India";
+    const userEmail = "testEmail@user.automation";
+    const pass = "Testthissite@123";
+
+
+    const lp = new loginPage(page);
+
+    await lp.goTo();
+    await lp.invalidCreds();
+    await lp.validCreds(userEmail,pass);
+
+
+    const dp = new dashboardPage();
+    await dp.productSelect(productName);
+
+    await page.locator('.cartSection').first().waitFor();
+    const bool = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("button[type='button']").last().click();
+
+    //Payment page
+
+    await page.locator(".field .input.txt.text-validated").fill("1234123412341234");
+    await page.locator(".input.ddl").first().selectOption("03");
+    await page.locator(".input.ddl").last().selectOption("27");
+
+    await page.locator(".field.small .input.txt").first().pressSequentially("098");
+    await paymentForm.nth(2).fill("Test User");
+    await paymentForm.last().fill("rahulshettyacademy");
+    await page.locator(".btn.btn-primary.mt-1").click();
+    await page.locator(".mt-1.ng-star-inserted").waitFor();
+    expect(page.locator(".mt-1.ng-star-inserted")).toContainText("Applied");
+
+    await page.locator('input[placeholder="Select Country"]').pressSequentially("ind");
+    await listCountry.first().waitFor();
+    const countryCount = await listCountry.count();
+    //console.log(countryCount);
+
+    for(let i=0; i<countryCount;++i){
+        if(await listCountry.nth(i).textContent() === nameCountry){
+            await listCountry.nth(i).click();
+            break;// come out of the loop after sekecting
+            
+
+        }
+   }
+
+   await expect(page.locator('.user__name.mt-5 label')).toHaveText(userEmail);
+   await page.locator('//a[text()="Place Order "]').waitFor();
+   await page.locator('//a[text()="Place Order "]').click();
+
+   //verifying the Thankyou message
+   await page.locator('.hero-primary').waitFor();
+   expect(page.locator('.hero-primary')).toHaveText(" Thankyou for the order. ");
+   const orderNo = await page.locator("label.ng-star-inserted").innerText();
+   let orderId = orderNo.split("|")[1];
+   orderId = orderId.trim();
+   console.log(orderId);
+
+   // verifying the order id
+
+   await page.locator("button[routerlink='/dashboard/myorders']").click();
+   console.log("In the orders list")
+   const orderList =  page.locator('tbody tr');
+   const listCount = await orderList.count();
+   console.log(listCount);
+    for(let i=0; i<listCount;++i){
+        const actualOrderId = await orderList.nth(i).locator('th').textContent();
+        console.log(actualOrderId);
+
+        if( orderId.includes(actualOrderId)){
+            await orderList.nth(i).locator('button.btn.btn-primary').click();
+            break;
+        }
+    }
+
+    //Verification of product order summary
+
+    await expect(page.locator('.tagline').first()).toHaveText("Thank you for Shopping With Us");
+  
+   await page.pause();
+    
+});
+
