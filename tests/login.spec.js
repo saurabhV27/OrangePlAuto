11bcc8963c877388bb0e377fd397c3fafd5a841a
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-09 15:44:36 +0530

    passing data through fixture

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,17 @@
 const{test,expect} = require("@playwright/test");
 const {POManager} = require('../PageLocators/POManager');
 const dataTest = JSON.parse(JSON.stringify(require('../testData.json')));
+const {customTest} = require("../test-fixture");
 
 
-test("Navigate to OrangeHRM", async({page})=>{
+test.only("Navigate to OrangeHRM", async({page})=>{
     
-    /*Data below is driven from external JSON*/ 
-    
-    // const productName = 'ZARA COAT 3'
-    // const nameCountry = " India";
-    // const userEmail = "testEmail@user.automation";
-    // const pass = "Testthissite@123";
-
     const manager = new POManager(page);
     const lp = manager.getLogin();
 
     await lp.goTo();
     await lp.invalidCreds();
     await lp.validCreds(dataTest.userEmail,dataTest.pass);
-
 
     const dp = manager.getDashboard();
     await dp.productSelect(dataTest.productName);
@@ -36,7 +29,56 @@ test("Navigate to OrangeHRM", async({page})=>{
     const mop = manager.getMyOrders();
     await mop.verifyOrderId(orderID);
     await mop.verifyTagLine();
-
     
 });
+
+
+customTest('customer fixture',async({browser,testDataFix})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const username = page.locator('#userEmail');
+    const password = page.locator('#userPassword');
+    const login = page.locator('#login');
+    const products = page.locator('.card-body');
+
+    
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    
+
+    await username.fill(testDataFix.userEmail);
+    await password.fill(testDataFix.pass);
+    await login.click();
+    console.log(await page.title());
+    expect(page).toHaveTitle("Let's Shop");
+
+    //Get the name of the first product
+
+    console.log(await products.first().textContent());
+    await products.first().waitFor();
+    const allElements = await products.allInnerTexts();
+    console.log(allElements);
+
+    const count = await products.count();
+    for(let i=0; i<count ; ++i)
+        {
+        if(await products.nth(i).locator('b').textContent() === testDataFix.productName)
+            {
+                //click on add to cart 
+                console.log("Clicking the add to cart button !!");
+                await products.nth(i).locator("text= Add To Cart").click();
+                break;
+    }
+    }
+
+    await page.locator('button[routerlink*="cart"]').click();
+    await page.locator('.cartSection').first().waitFor();
+    const bool = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("button[type='button']").last().click();
+
+
+
+
+
+})
 
